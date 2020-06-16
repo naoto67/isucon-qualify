9b372790f868749971771518a82715d7abe852a4
naoto67 <uver.com67@gmail.com>
2020-06-17 07:34:25 +0900

    itemsテーブルにseller_id, buyer_id関連のインデックス追加 300~500程度上昇

--- sql/03_alters.sql
+++ sql/03_alters.sql
@@ -1,1 +1,3 @@
 ALTER TABLE items ADD INDEX created_at_idx(`created_at`);
+ALTER TABLE items ADD INDEX seller_id_and_created_at_idx(`seller_id`, `created_at`);
+ALTER TABLE items ADD INDEX buyer_id_idx(`buyer_id`);
