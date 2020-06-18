bb5b1a58c79fc7a2c30d6972f5e122af589cbf5d
naoto67 <uver.com67@gmail.com>
2020-06-18 13:33:32 +0900

    7000番portで待っているredisを7006番に変更

--- go/redis.go
+++ go/redis.go
@@ -14,7 +14,7 @@ var redisCluster redis.Cluster
 func newRedis() (redis.Cluster, error) {
 	return redis.NewCluster(
 		&redis.Options{
-			StartNodes:   []string{"127.0.0.1:7000", "127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003", "127.0.0.1:7004", "127.0.0.1:7005"},
+			StartNodes:   []string{"127.0.0.1:7006", "127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003", "127.0.0.1:7004", "127.0.0.1:7005"},
 			ConnTimeout:  50 * time.Millisecond,
 			ReadTimeout:  50 * time.Millisecond,
 			WriteTimeout: 50 * time.Millisecond,
