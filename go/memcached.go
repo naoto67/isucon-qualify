09a34668bc587903aecac1db4295eb13489e5a8c
naoto67 <uver.com67@gmail.com>
2020-06-22 16:29:08 +0900

    memcachedの接続を1台から2台に変更

--- go/memcached.go
+++ go/memcached.go
@@ -4,26 +4,40 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-const memcachedAddress = "localhost:11211"
+const memcached1Address = "localhost:11211"
+const memcached2Address = "localhost:11212"
 
 type CacheClient struct {
-	cli *memcache.Client
+	cli1 *memcache.Client
+	cli2 *memcache.Client
 }
 
 var cacheClient CacheClient
 
 func NewCacheClient() {
-	cli := memcache.New(memcachedAddress)
-	cli.MaxIdleConns = 10
-	cacheClient = CacheClient{cli: cli}
+	cli1 := memcache.New(memcached1Address)
+	cli2 := memcache.New(memcached2Address)
+	cli1.MaxIdleConns = 10
+	cli2.MaxIdleConns = 10
+	cacheClient = CacheClient{cli1: cli1, cli2: cli2}
 }
 
 func (c CacheClient) Set(key string, value []byte) error {
-	return c.cli.Set(&memcache.Item{Key: key, Value: value})
+	if len(key)%2 == 0 {
+		return c.cli1.Set(&memcache.Item{Key: key, Value: value})
+	} else {
+		return c.cli2.Set(&memcache.Item{Key: key, Value: value})
+	}
 }
 
 func (c CacheClient) Get(key string) ([]byte, error) {
-	item, err := c.cli.Get(key)
+	var item *memcache.Item
+	var err error
+	if len(key)%2 == 0 {
+		item, err = c.cli1.Get(key)
+	} else {
+		item, err = c.cli2.Get(key)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -31,9 +45,14 @@ func (c CacheClient) Get(key string) ([]byte, error) {
 }
 
 func (c CacheClient) Delete(key string) error {
-	return c.cli.Delete(key)
+	if len(key)%2 == 0 {
+		return c.cli1.Delete(key)
+	} else {
+		return c.cli2.Delete(key)
+	}
 }
 
 func (c CacheClient) FLUSH() {
-	c.cli.FlushAll()
+	c.cli1.FlushAll()
+	c.cli2.FlushAll()
 }
